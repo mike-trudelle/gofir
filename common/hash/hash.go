81dea6706e061fb73b39ad2249025fad939b5eb8
Mike Trudelle <mike.trudelle@gmail.com>
2019-11-24 21:20:25 -0500

    Added code for common hashing library

---
+++ common/hash/hash.go
@@ -0,0 +1,53 @@
+package hash
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"os"
+	"strings"
+)
+
+func getHashValue(s string, a string) string {
+
+	f, err := os.Open(s)
+
+	if err != nil {
+		//some error logging
+	}
+
+	defer f.Close()
+
+	var hvalue string
+
+	switch a {
+	case "MD5":
+		h := md5.New()
+		if _, err := io.Copy(h, f); err != nil {
+			//some error logging
+		}
+		hvalue = convByteArrToUcString(h.Sum(nil)[:])
+
+	case "SHA1":
+		h := sha1.New()
+		if _, err := io.Copy(h, f); err != nil {
+			//some error logging
+		}
+		hvalue = convByteArrToUcString(h.Sum(nil)[:])
+
+	case "SHA256":
+		h := sha256.New()
+		if _, err := io.Copy(h, f); err != nil {
+			//some error logging
+		}
+		hvalue = convByteArrToUcString(h.Sum(nil)[:])
+	}
+
+	return hvalue
+}
+
+func convByteArrToUcString(b []byte) string {
+	return strings.ToUpper(hex.EncodeToString(b))
+}
