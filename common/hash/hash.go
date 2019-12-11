ce21466af4bc981c077862bd75ae1fad33d154ee
Mike Trudelle <mike.trudelle@gmail.com>
2019-12-10 21:58:09 -0500

    added error as part of output - push error to main package

--- common/hash/hash.go
+++ common/hash/hash.go
@@ -12,37 +12,40 @@ import (
 )
 
 // GetHashValue gets the hash of a string using a chosen algorithm
-func GetHashValue(s string, a string) string {
+func GetHashValue(s string, a string) (string, error) {
 
+	var hvalue string
 	f, err := os.Open(s)
 
 	if err != nil {
-		//some error logging
+		hvalue = ""
+		return hvalue, err
 	}
 
 	defer f.Close()
-
-	var hvalue string
 
 	switch a {
 	case "MD5":
 		h := md5.New()
 		if _, err := io.Copy(h, f); err != nil {
-			//some error logging
+			hvalue = ""
+			return hvalue, err
 		}
 		hvalue = convByteArrToUcString(h.Sum(nil)[:])
 
 	case "SHA1":
 		h := sha1.New()
 		if _, err := io.Copy(h, f); err != nil {
-			//some error logging
+			hvalue = ""
+			return hvalue, err
 		}
 		hvalue = convByteArrToUcString(h.Sum(nil)[:])
 
 	case "SHA256":
 		h := sha256.New()
 		if _, err := io.Copy(h, f); err != nil {
-			//some error logging
+			hvalue = ""
+			return hvalue, err
 		}
 		hvalue = convByteArrToUcString(h.Sum(nil)[:])
 
@@ -50,19 +53,21 @@ func GetHashValue(s string, a string) string {
 		tablePolynomial := crc32.MakeTable(0xedb88320)
 		h := crc32.New(tablePolynomial)
 		if _, err := io.Copy(h, f); err != nil {
-			//some error logging
+			hvalue = ""
+			return hvalue, err
 		}
 		hvalue = convByteArrToUcString(h.Sum(nil)[:])
 
 	default:
 		h := md5.New()
 		if _, err := io.Copy(h, f); err != nil {
-			//some error logging
+			hvalue = ""
+			return hvalue, err
 		}
 		hvalue = convByteArrToUcString(h.Sum(nil)[:])
 	}
 
-	return hvalue
+	return hvalue, nil
 }
 
 func convByteArrToUcString(b []byte) string {
